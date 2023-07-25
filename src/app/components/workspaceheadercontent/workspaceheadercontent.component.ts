77e4af6563cd0de5b045c1799db6492d07cb56ab
niels-2005 <niels.scholz01@web.de>
2023-07-25 17:24:15 +0200

    added content

--- src/app/components/workspaceheadercontent/workspaceheadercontent.component.ts
+++ src/app/components/workspaceheadercontent/workspaceheadercontent.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class WorkspaceheadercontentComponent {
 
+  showProfileLogoutContainer(){
+    document.getElementById('opacity-background')?.classList.remove('d-none');
+    document.getElementById('profile-logout')?.classList.remove('d-none');
+  }
+
+  hideProfileLogoutContainer(){
+    document.getElementById('opacity-background')?.classList.add('d-none');
+    document.getElementById('profile-logout')?.classList.add('d-none');
+  }
+
+  showProfileDetailsContainer(){
+    document.getElementById('opacity-background-profile-details')?.classList.remove('d-none');
+    document.getElementById('profile-details')?.classList.remove('d-none');
+  }
+
+  hideProfileDetailsContainer(){
+    document.getElementById('opacity-background-profile-details')?.classList.add('d-none');
+    document.getElementById('profile-details')?.classList.add('d-none');
+    document.getElementById('edit-profile')?.classList.add('d-none');
+  }
+
+  showEditProfileContainer(){
+    document.getElementById('profile-details')?.classList.add('d-none');
+    document.getElementById('edit-profile')?.classList.remove('d-none');
+  }
+
+
 }
